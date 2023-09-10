b154838e61b07f380d6829a0ceed2dc4fb5a5dcd
James Posey <James.C.Posey@gmail.com>
2023-09-10 13:13:57 -0400

    Added fireworks to section 2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Define objects
     const sections = document.querySelectorAll('section');
     const navbar = document.getElementById('navbar__list');
-    const fireworksContainer = document.querySelector('.fireworks-container');
+    const collapsibleButtons = document.querySelectorAll('.collapsible');
 
     // Function to create the navigation bar dynamically
     function createNavbar() {
@@ -44,28 +44,73 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
     
-    // Function to create fireworks effect
-    function createFirework() {
-      const firework = document.createElement('div');
-      firework.classList.add('firework');
-      firework.style.left = `${Math.random() * 100}%`;
-      firework.style.top = `${Math.random() * 100}%`;
-      fireworksContainer.appendChild(firework);
-  
-      setTimeout(() => {
-        firework.remove();
-      }, 2000);
-    }
-  
-    function launchFireworks() {
-      setInterval(createFirework, 3000); // Launch fireworks every 3 seconds
-    }
-  
-    // Trigger fireworks on section load
-    if (fireworksContainer) {
-      launchFireworks();
-    }
+ // Function to create a firework particle
+ function Particle(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 2 + 1;
+    this.speedY = -Math.random() * 3;
+    this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  }
 
+  // Function to draw a firework particle
+  Particle.prototype.draw = function () {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  };
+
+  // Function to create explosions
+  function createExplosion(x, y) {
+    for (let i = 0; i < 100; i++) {
+      const particle = new Particle(x, y);
+      particles.push(particle);
+    }
+  }
+
+  // Function to animate the fireworks
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach((particle, index) => {
+      if (particle.size <= 0.2) {
+        particles.splice(index, 1);
+      } else {
+        particle.size -= 0.05;
+        particle.y += particle.speedY;
+        particle.draw();
+      }
+    });
+    requestAnimationFrame(animate);
+  }
+
+  // Event listener for fireworks
+  document.getElementById('section2').addEventListener('mouseenter', function () {
+    const fireworksContainer = this.querySelector('.fireworks-container');
+    const rect = this.getBoundingClientRect();
+    const x = rect.left + Math.random() * rect.width;
+    const y = rect.top + Math.random() * rect.height;
+
+    const firework = document.createElement('div');
+    firework.classList.add('firework');
+    firework.style.left = x + 'px';
+    firework.style.top = y + 'px';
+    fireworksContainer.appendChild(firework);
+
+    setTimeout(() => {
+        firework.remove();
+    }, 2000);
+});
+  
+  // triggers buttons  
+    collapsibleButtons.forEach(button => {
+        button.addEventListener('click', function () {
+          const content = this.nextElementSibling;
+          content.style.display = content.style.display === 'block' ? 'none' : 'block';
+        });
+      });
+    
     // Add event listeners
     window.addEventListener('scroll', setActiveSection);
     navbar.querySelectorAll('.menu__link').forEach(anchor => {
